5ea2a2e55267d43a45f70e0fb3e3d321994f0ac9
D4_80469_SiddheshPatil <patilsiddhesh189@gmail.com>
2023-09-08 22:21:23 +0530

    deleting testing files

--- selftry.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-    cout<<"Self try";
-    
-    return 0;
-}
--- temp.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-    cout<<"self try push";
-    return 0;
-}
