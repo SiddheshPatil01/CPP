992146c1374a2682a83b56fa19f4a16dde0b3d98
D4_80469_SiddheshPatil <patilsiddhesh189@gmail.com>
2023-09-17 18:34:19 +0530

    r

--- assignment 4/q3.cpp
+++ assignment 4/q3.cpp
@@ -1,6 +1,184 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    return 0;
+class product
+{
+private:
+  int id;
+  string title;
+
+public:
+  product()
+  {
+    this->id = 0;
+    this->title = " ";
+  }
+  product(int id, string title)
+  {
+    this->id = id;
+    this->title = title;
+  }
+  virtual void acceptdata()
+  {
+    cout << "enter id : " << endl;
+    cin >> this->id;
+
+    cout << "enter title : " << endl;
+    cin >> this->title;
+  }
+
+  virtual int getvalue() = 0;
+
+  virtual void printdata()
+  {
+    cout << "id  :" << this->id << endl;
+    cout << "title  :<<" << this->title << endl;
+  }
+};
+
+class book : public product
+{
+private:
+  string author;
+  int price;
+
+public:
+  int total = 0;
+  book()
+  {
+    this->author = " ";
+    this->price = 0;
+  }
+  book(string author, int price)
+  {
+
+    this->author = author;
+    this->price = price;
+  }
+  void acceptdata()
+  {
+    product ::acceptdata();
+    cout << "enter author : " << endl;
+    cin >> this->author;
+
+    cout << "enter price : " << endl;
+    cin >> this->price;
+  }
+
+  void printdata()
+  {
+    product ::printdata();
+    cout << "author : " << this->author << endl;
+    cout << "price : " << this->price << endl;
+  }
+  int getvalue()
+  {
+    return ((this->price) - (this->price / 5));
+  }
+};
+class tape : public product
+{
+private:
+  string artist;
+  int price;
+
+public:
+  tape()
+  {
+    this->artist = " ";
+    this->price = 0;
+  }
+  void acceptdata()
+  {
+    product ::acceptdata();
+    cout << "enter artist : " << endl;
+    cin >> this->artist;
+
+    cout << "enter price : " << endl;
+    cin >> this->price;
+  }
+
+  void printdata()
+  {
+    product ::printdata();
+    cout << "artist : " << this->artist << endl;
+    cout << "price : " << this->price << endl;
+  }
+  int getvalue()
+  {
+    return ((this->price) - (this->price / 10));
+  }
+
+  
+};
+
+int menu()
+{
+  int choice;
+  cout << "------------------------" << endl;
+  cout << "0.Exit......" << endl;
+  cout << "1.accept book" << endl;
+  cout << "2.accept tape" << endl;
+  cout << "3 print data" << endl;
+  cout << "4 total bill " << endl;
+  cin >> choice;
+  cout << "------------------------" << endl;
+  return choice;
 }
+
+int main()
+{
+
+  product *ptr[3];
+  int choice;
+  int index = 0;
+  int max = 3;
+  int total;
+
+  while ((choice = menu()) != 0)
+  {
+    switch (choice)
+    {
+    case 1:
+      if (index < max)
+      {
+        ptr[index] = new book();
+        ptr[index]->acceptdata();
+        index++;
+      }
+      else
+      {
+        cout << "Stack is a full" << endl;
+      }
+      break;
+
+    case 2:
+      if (index < max)
+      {
+        ptr[index] = new tape();
+        ptr[index]->acceptdata();
+        index++;
+      }
+      else
+      {
+        cout << "it is a full" << endl;
+      }
+      break;
+
+    case 3:
+      for (int i = 0; i < max; i++)
+      {
+        ptr[i]->printdata();
+      }
+      break;
+
+    case 4:
+      int totalbill = 0;
+      for (int i = 0; i < max; i++)
+      {
+        totalbill += ptr[i]->getvalue();
+      }
+      cout << " total bill" << totalbill << endl;
+    }
+  }
+};
---
+++ assignment 5/q1.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+class Distance{
+    private:
+    int feet;
+    int inchs;
+
+    public:
+        Distance(){
+            this->feet=0;
+            this->inchs=0;
+        }
+        Distance(int feet, int inchs){
+            this->feet=feet;
+            this->inchs=inchs;
+        }
+        Distance operator+(Distance &other){
+            Distance d3;
+            d3.feet = this->feet + other.feet;
+            d3.inchs = this->inchs + other.inchs;
+            return d3;
+        }
+        
+
+        friend void operator<<(ostream &out, Distance &other );
+        friend void operator>>(istream &in, Distance &other );
+        friend bool operator==(Distance &d1, Distance &d2);
+};
+
+void operator<<(ostream &out, Distance &other ){
+    cout<<other.feet<<" feet "<<other.inchs<<" inchs "<<endl;
+}
+
+void operator>>(istream &in, Distance &other){
+    
+    cout<<"Enter Feet: ";
+    cin>>other.feet;
+    cout<<"Enter inchs";
+    cin>>other.inchs;
+}
+
+bool operator==(Distance &d1, Distance &d2){
+    if(d1.feet == d2.feet && d1.inchs==d2.inchs){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+int main(){
+    Distance d1(1,2);
+    Distance d2(1,2);
+    Distance d3 = d1 + d2;
+    cout<<d3;
+    if(d1 == d2){
+        cout<<"Objects area equal"<<endl;
+    }else{
+        cout<<"Not equal"<<endl;
+    }
+    return 0;
+}
